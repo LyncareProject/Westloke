8da3a40edf692ee5527631ada2fe319e1ef21eec
Kun Woo Kim <kimkuns98@gmail.com>
2023-09-01 15:59:14 +0900

    refixed

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.enable('trust proxy');
-// app.use(function(req, res, next){
-//         if(!req.secure){
-//                 res.redirect("https://"+ req.headers.host + req.url);
-//         }else{
-//                 next();
-//         }
-// });
+app.enable('trust proxy');
+app.use(function(req, res, next){
+        if(!req.secure){
+                res.redirect("https://"+ req.headers.host + req.url);
+        }else{
+                next();
+        }
+});
 
 const apiRouter = require('./routes/index');
 
