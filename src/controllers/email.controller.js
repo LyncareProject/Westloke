694c9d43da3cb89dc33ad3296bb0566e4e641023
Kun Woo Kim <kimkuns98@gmail.com>
2023-09-04 12:46:19 +0900

    반응형

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -16,72 +16,74 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.postEmail = async (req, res) => {
-    const { email, Model, Price } = req.body
-    const handleModel = ()=>{
-        if(Model === 'Model 120'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model120.png"
+    try {
+        const { email, Model, Price } = req.body
+        const handleModel = ()=>{
+            if(Model === 'Model 120'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model120.png"
+            }
+            if(Model === 'Model 60'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model60.png"
+            }
+            if(Model === 'Combo 12” x1'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Combo12.png"
+            }
+            if(Model === 'Combo 10” x2'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Combo10.png"
+            }
+            if(Model === 'Head'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Head.png"
+            }
+            if(Model === 'Head w/ 15” x1 speaker cabinet'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Head15_x1.png"
+            }
+            if(Model === 'Head w/ 10”x4 speaker cabinet'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Head10_x4.png"
+            }
+            if(Model === 'Model C4'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model_C4.png"
+            }
+            if(Model === 'Model 15B'){
+                return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model_15B.png"
+            }
         }
-        if(Model === 'Model 60'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model60.png"
-        }
-        if(Model === 'Combo 12” x1'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Combo12.png"
-        }
-        if(Model === 'Combo 10” x2'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_Combo10.png"
-        }
-        if(Model === 'Head'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30.png"
-        }
-        if(Model === 'Head w/ 15” x1 speaker cabinet'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30.png"
-        }
-        if(Model === 'Head w/ 10”x4 speaker cabinet'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model30_HeadW10.png"
-        }
-        if(Model === 'Model C4'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model_C4.png"
-        }
-        if(Model === 'Model 15B'){
-            return "https://wevibebucket.s3.ap-northeast-2.amazonaws.com/Model_15B.png"
-        }
+        await transporter.sendMail({
+            from: EMAIL_USER, // sender address
+            // to: [ email ], // list of receivers
+            to: [ email ,EMAIL_USER ], // list of receivers
+            subject: "Westloke Product Inquiries", // Subject line
+            html: `<div style='
+                        width: 100%;
+                        min-height: 1300px
+                    '>
+                <div style='
+                        text-align: center;
+                        width: 800px;
+                        margin: 30px auto;
+                        padding: 40px 80px;
+                        border: 1px solid #EDEDED;
+                        background: #FFF;
+                        box-sizing: border-box;
+                    '>
+                    <h3 style='font-size : 26px; font-weight : 400; margin-bottom : 30px;'>Westloke</h3> 
+                    <h2 style='font-size : 26px; font-weight : 800; margin-bottom : 30px;'>Your Order has been Received Succesfully</h2> 
+                    <p style='font-size : 18px; margin-bottom : 30px; color: #CACACA'>Order By <span style='font-weight : 600; margin-right: 10px;'>${ email }</span></p>      
+                    <h2 style='font-size : 18px; font-weight : 800; margin-bottom : 30px;color: #000;'>You just Saved <span style='color : #CAAC72;'>$ ${ (parseInt(Price) - parseInt(parseInt(Price) * 0.85)).toLocaleString() }</span></h2> 
+                    <img style='margin-bottom : 30px;' src=${ handleModel() } alt="Inline Image" />
+                    <p style='font-size : 16px; font-weight : 600;'>Made in Redmond,WA</p> 
+                    <p style='font-size : 14px; color : #989898;'>Lifetime Warranty for Amp itself<br>5 Years Warranty for the Tubes</p>
+                </div>
+            </div>`,
+        })
+            .then(response => {
+                console.log('Email sent: ' + response.response)
+                res.status(200).json({message : 'Success'})
+            })
+            .catch(error=>console.log(error))
+    } catch (error) {
+        console.error('Error during signing:', error);
+        res.status(500).json({ message: 'Server error' });
     }
-    await transporter.sendMail({
-        from: EMAIL_USER, // sender address
-        // to: [ email ], // list of receivers
-        to: [ email ,EMAIL_USER ], // list of receivers
-        subject: "Westloke Product Inquiries", // Subject line
-        html: `<div style='
-                    width: 100%;
-                    min-height: 1300px
-                '>
-            <div style='
-                    text-align: center;
-                    width: 800px;
-                    margin: 30px auto;
-                    padding: 40px 80px;
-                    border: 1px solid #EDEDED;
-                    background: #FFF;
-                    box-sizing: border-box;
-                '>
-                <h3 style='font-size : 26px; font-weight : 400; margin-bottom : 30px;'>Westloke</h3> 
-                <h2 style='font-size : 26px; font-weight : 800; margin-bottom : 30px;'>Your Order has been Received Succesfully</h2> 
-                <h2 style='font-size : 16px; font-weight : 800; margin-bottom : 10px; color: #989898;'>The original price is $ ${ parseInt(Price).toLocaleString() }, but you bought it at a discounted price $ ${parseInt(parseInt(Price) * 0.85).toLocaleString() }. </h2> 
-                <h2 style='font-size : 20px; font-weight : 800; margin-bottom : 30px;color: #777;'>You saved a total of <span style='color : #EC0000;'>$ ${ (parseInt(Price) - parseInt(parseInt(Price) * 0.85)).toLocaleString() }</span></h2> 
-                <p style='font-size : 18px; margin-bottom : 30px'>Order By <span style='font-weight : 600; margin-right: 10px;'>${ email }</span></p>      
-                <img style='margin-bottom : 30px;' src=${ handleModel() } alt="Inline Image" />
-                <p style='font-size : 16px; font-weight : 600;'>Made in Redmond,WA</p> 
-                <p style='font-size : 14px; color : #989898;'>Lifetime Warranty for Amp itself<br>5 Years Warranty for the Tubes</p>
-            </div>
-        </div>`,
-    })
-    
-    .then(response => {
-        console.log('Email sent: ' + response.response)
-        res.status(200).json({message : 'Success'})
-    })
-    .catch(error=>console.log(error))
-
 }
 exports.postContactEmail = async (req, res) => {
     try {
